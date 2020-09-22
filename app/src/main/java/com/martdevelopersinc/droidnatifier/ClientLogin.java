d190b15f90afa45961a82301abefb35cf56db0a5
MartMbithi <martdevelopers254@gmail.com>
2020-09-22 12:45:08 +0300

    Cranked Client Login Activity

---
+++ app/src/main/java/com/martdevelopersinc/droidnatifier/ClientLogin.java
@@ -0,0 +1,14 @@
+package com.martdevelopersinc.droidnatifier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ClientLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_login);
+    }
+}
